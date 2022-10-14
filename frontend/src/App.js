eb057e035ae882bd54b5da90de0be0aa2214fd8c
Aans Rehman Khan <aansrehman10@gmail.com>
2022-10-14 21:06:05 +0500

    changes in ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
+// import React, {useState} from 'react';
 function App() {
+
+  const [data , setData] = useState(null)
+  const [print , setPrint] = useState(false)
+
+  function getData(val)
+  {
+    setData(val.target.value)
+    setPrint(false)
+    console.warn(val.target.value)
+  }
   return (
     <div className="App">
+      {
+        print?
+        <h1>{data}</h1>
+        :null
+      }
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/* <h3>Enter your Name: {data} </h3> */}
+        <input type="txet" onChange={getData}/>
+        <br/>
+        <button onClick={()=>setPrint(true)}>Print Txt</button>
+        </header>
     </div>
   );
 }
