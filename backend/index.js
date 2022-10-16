f51a4677a17d60bb43518d7ca7b547b02a96f601
Aans Rehman Khan <aansrehman10@gmail.com>
2022-10-16 15:04:22 +0500

    backend complete except ui integration

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@
 import express from 'express'; // <-- Module Style import
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-
+import cors from 'cors';
 import UserModel from './db/users.js';
 
 // Importing user route
@@ -16,10 +16,14 @@ import router from './routes/users.js';
 
 // const bodyParser = require('body-parser')
 
+
+// const cors = require('cors');
+
 const app = express()
 const port = 3001
-
-mongoose.connect('mongodb+srv://Aans:Aans123@cluster0.0qgjt.mongodb.net/?retryWrites=true&w=majority',
+app.use(express.json())
+app.use(cors());
+mongoose.connect('mongodb+srv://Aans:Aans123@cluster0.56vbw6b.mongodb.net/test',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -44,11 +48,23 @@ app.get('/getUsers', (req, res) => {
     }
     else{
       res.json(result);
+      console.log(`Success`);
+
     }
   })
 })
 
+app.post('/createUser', async (req, res)=>{
+  const user = req.body;
+  const newUser = new UserModel(user);
+  await newUser.save();
 
+  res.json(user);
+
+  console.log(newUser.username + ' stored in DB')
+})
+
+ 
 app.get('/todos', (req, res) => {
     res.send('A list of todo items will be returned')
 })
