0ced317ed999861a0b4903e0310824eeb4571818
Aans Rehman Khan <aansrehman10@gmail.com>
2022-10-16 22:21:22 +0500

    completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import React from 'react';
 import { useEffect, useState } from 'react';
 import Axios from 'axios';
-// import React, {useState} from 'react';
+
 function App() {
 
  const [listofUsers, setlistofUsers] = useState([]);
+ const [username , setUsername] = useState("");
 
   useEffect(()=>{
     Axios.get("http://localhost:3001/getUsers").then((response)=> {
@@ -14,56 +15,37 @@ function App() {
     })
   }, []);
 
-  // const [data , setData] = useState(null)
-  // const [print , setPrint] = useState(false)
-
-  // function getData(val)
-  // {
-  //   setData(val.target.value)
-  //   setPrint(false)
-  //   console.warn(val.target.value)
-  // }
+const createUser=()=>{
+  Axios.post("http://localhost:3001/createUser", {username,}).then((response)=>{
+    alert("USER CREATED!");
+  });
+}
   return (
     <div className="App">
-
-
-      {/* <div className="usersDiplay">
+      <div className="usersDiplay">
+        <h4>The users in DB are:</h4>
         {listofUsers.map((user)=>{
           return(
             <div>
-              <h1>Name: {user.username}</h1>
+              <h5>{user.username}</h5>
             </div>
           )
         })}
-        </div> */}
-      
-       <header className="App-header">
-       {/* <h3>Enter your Name: {data} </h3> */}
-       {
-       listofUsers.map((user)=>{
-          return(
-            <div>
-              <h1>Name: {user.username}</h1>
-            </div>
-          )
-        })
-        }
-       </header>
+        </div> 
+      <header className="App-header">
+        <h3>Enter your Name: </h3>
+        <input type="txet" onChange={(event)=>{
+          setUsername(event.target.value);
+        }}/>
+        <br/>
+        <button onClick={
+          createUser
+          }>Insert in DB</button>
+    
+        </header>
+    
+    </div>
 
-      
-        </div>
-    //   {
-    //     print?
-    //     <h1>{data}</h1>
-    //     :null
-    //   }
-    //   <header className="App-header">
-    //     {/* <h3>Enter your Name: {data} </h3> */}
-    //     <input type="txet" onChange={getData}/>
-    //     <br/>
-    //     <button onClick={()=>setPrint(true)}>Print Txt</button>
-    //     </header>
-    // </div>
   );
 }
 
